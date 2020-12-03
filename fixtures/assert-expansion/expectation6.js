4ff9d2f3bd42c14a30a0e994f6c7866fb7c33850
NullVoxPopuli <LPSego3+dev@gmail.com>
2020-12-03 10:43:12 -0500

    Add failing test for when renamed imports are not transpiled to the correct thing

--- fixtures/assert-expansion/expectation6.js
+++ fixtures/assert-expansion/expectation6.js
@@ -1,2 +1,3 @@
 (true && !((() => true)()) && console.assert((() => true)(), 'This is an assertion'));
 (true && !(false) && console.assert(false, 'This is an assertion 2'));
+(true && !(false) && console.assert(false, 'renamed assertion'));
