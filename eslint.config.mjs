bd915a621c4707f1d5c8a09e4f9dac99df6bf956
Edward Faulkner <edward@eaf4.com>
2024-04-06 12:17:53 -0400

    switching to pnpm and updating many deps

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier/recommended';
+import node from 'eslint-plugin-n';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  prettier,
+  node.configs['flat/recommended-module'],
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+    ignores: ['/fixtures/'],
+  },
+  {
+    files: ['tests/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+  },
+];
