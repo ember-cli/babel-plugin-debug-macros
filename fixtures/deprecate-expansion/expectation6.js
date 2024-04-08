ed9221c678cf072d478d0823fa04a734117877b1
Edward Faulkner <edward@eaf4.com>
2024-04-08 11:18:28 -0400

    dropping babel 6

--- fixtures/deprecate-expansion/expectation6.js
+++
@@ -1,7 +0,0 @@
-(true && !(true) && console.warn('This is deprecated'));
-(true && !(false) && console.warn('Message without predicate'));
-(true && !(true) && console.warn('This is deprecated with options'));
-
-function wrappedDeprecate() {
-  (true && !(true) && console.warn('This is also deprecated'));
-}
