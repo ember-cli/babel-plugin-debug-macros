5e62e9600983dcbd9ced11bad190a383e99f732a
Edward Faulkner <edward@eaf4.com>
2024-04-10 18:07:45 -0400

    re-add utils-macros-test

--- tests/utils-macros-test.js
+++ tests/utils-macros-test.js
@@ -1,11 +1,8 @@
 'use strict';
 
-import { createRequire } from 'node:module';
-
-const require = createRequire(import.meta.url);
 const macros = require('../src/index.js');
 const { transformSync } = require('@babel/core');
-import { expect, describe, it } from 'chai';
+const { expect } = require('chai');
 
 describe('utils/macros.js', function () {
   it('does not fail if an import intended to be removed has already been removed', function () {
